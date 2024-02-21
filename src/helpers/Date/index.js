dd08fd12f7d54254570ac64ca0a543c9a9b3bf24
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-02-21 13:34:50 +0100

    fix component date

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -13,4 +13,16 @@ export const MONTHS = {
   12: "décembre",
 };
 
-export const getMonth = (date) => MONTHS[date.getMonth()];
+export const getMonth = (date) => {
+  if (!(date instanceof Date)) {
+    /* Si date n'est pas une instance de Date valide, 
+    retournez une chaîne vide ou une valeur par défaut */
+    return "";
+  }
+
+  const monthIndex = date.getMonth();
+  if (monthIndex >= 0 && monthIndex <= 11) {
+    return MONTHS[monthIndex + 1]; // Ajoutez 1 pour obtenir le mois correct dans MONTHS
+  }
+    return "";
+  };
