a62121ec51d1d67c9f7e42c4c9aded00a9af5046
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-02-21 21:31:20 +0100

    fix bug filter : ajout filter sur data.events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -4,19 +4,20 @@ import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
 import Modal from "../Modal";
 import ModalEvent from "../ModalEvent";
-
 import "./style.css";
 
 const PER_PAGE = 9;
 
 const EventList = () => {
-  const { data, error } = useData();
-  const [type, setType] = useState();
-  const [currentPage, setCurrentPage] = useState(1);
+  const { data, error } = useData(); 
+  const [type, setType] = useState();  
+  const [currentPage, setCurrentPage] = useState(1); 
+
+  // Filtrage des événements en fonction du type sélectionné
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter(event => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
@@ -26,12 +27,15 @@ const EventList = () => {
     }
     return false;
   });
+
   const changeType = (evtType) => {
-    setCurrentPage(1);
-    setType(evtType);
+    setCurrentPage(1);  
+    setType(evtType);  
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+  const typeList = new Set(data?.events.map((event) => event.type));  
+
   return (
     <>
       {error && <div>An error occured</div>}
