dd40726d377bd8c650e7b2b38854a5b22dfed269
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-02-21 13:35:51 +0100

    fix bug filter

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -15,10 +15,11 @@ const Select = ({
 }) => {
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
+  
   const changeValue = (newValue) => {
-    onChange();
+    onChange(newValue);
     setValue(newValue);
-    setCollapsed(newValue);
+    setCollapsed(!collapsed);
   };
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
