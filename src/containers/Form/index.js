1f4a80f8ebbe238b24bdc8d73b481e21a3c4c4b0
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-02-22 19:05:01 +0100

    fix bug form : add the OnSuccess function call

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,7 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 800); })
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -12,10 +12,12 @@ const Form = ({ onSuccess, onError }) => {
     async (evt) => {
       evt.preventDefault();
       setSending(true);
+      
       // We try to call mockContactApi
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess() // Appeler la fonction onSuccess après un envoi réussi
       } catch (err) {
         setSending(false);
         onError(err);
