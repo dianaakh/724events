727495798756fa7e66231f0d26b72d1f84f800e5
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-02-22 19:01:13 +0100

    fix bug slider, no more blank page + pag.corrected

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
@@ -7,53 +7,67 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+  
+  // Utilisation de useMemo pour calculer byDateAsc seulement lorsque data change
+  const byDateAsc = useMemo(() => {
+    return data?.focus?.sort((evtA, evtB) =>
+      new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
+    ) || [];
+  }, [data]);
+
   useEffect(() => {
-    nextCard();
-  });
+    /* Utilisation de setInterval pour déclencher périodiquement 
+    le changement de diapositive */
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % byDateAsc.length);
+    }, 5000);
+
+    // Nettoyage de l'intervalle lors du démontage du composant
+    return () => clearInterval(interval);
+  }, [byDateAsc]);
+
+  // Fonction pour gérer les clics sur les points de pagination
+  const handleBulletPointClick = (clickedIndex) => {
+    setIndex(clickedIndex);
+  };
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
+      {byDateAsc.map((event, idx) => (
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
             }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateAsc.map((_, radioIdx) => (
+            <input
+            /* Utilisation d'une clé unique basée sur l'index */
+              key={`radio-${radioIdx}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => { }}
+              /* Gestion de l'événement onClick pour mettre à jour l'index */
+              onClick={() => handleBulletPointClick(radioIdx)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Slider;
+
